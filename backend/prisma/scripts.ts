c35c6074e09df2a67c68fac142673133be709c60
filip <filip@smartiot.cloud>
2024-10-30 16:55:51 +0200

    populate with dummy data

--- backend/prisma/scripts.ts
+++ backend/prisma/scripts.ts
@@ -4,16 +4,26 @@ const prisma = new PrismaClient();
 // https://passwordsgenerator.net/sha256-hash-generator/ generate hash for testing purposes
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      username: "filip",
-      email: "testfilip@mail.com",
-      passwordHash:
-        "5994471ABB01112AFCC18159F6CC74B4F511B99806DA59B3CAF5A9C173CACFC5",
-    },
-  });
-
-  console.log(user);
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       username: "test_user",
+  //       email: "test_user@test.com",
+  //       passwordHash:
+  //         "5994471ABB01112AFCC18159F6CC74B4F511B99806DA59B3CAF5A9C173CACFC5",
+  //     },
+  //   });
+  //  const users = await prisma.user.findMany();
+  //   const comment = await prisma.comment.create({
+  //     data: {
+  //       commentText: "this is my first comment",
+  //       date: new Date("2022-05-22T22:30:00Z"),
+  //       likes: 293,
+  //       dislikes: 9232,
+  //       userId: 1,
+  //     },
+  //   });
+  // const comments = await prisma.comment.findMany();
+  // console.log(comments);
 }
 
 main()
