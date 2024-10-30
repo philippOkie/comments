c585c1274b528aac0f8659eba14206cf298b7b7a
filip <filip@smartiot.cloud>
2024-10-30 16:47:39 +0200

    unused

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,4 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../db/queries");
-
-router.get("/", async (req, res) => {
-  try {
-    const usernames = await db.getAllUsernames();
-    res.json({ users: usernames });
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
 module.exports = router;
