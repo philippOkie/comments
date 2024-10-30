2f8b82b7f4a6ebf4f3eae295e9411b703aa1c60a
filip <filip@smartiot.cloud>
2024-10-30 16:47:46 +0200

    updated scripts ts

--- backend/prisma/scripts.ts
+++ backend/prisma/scripts.ts
@@ -1,6 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+// https://passwordsgenerator.net/sha256-hash-generator/ generate hash for testing purposes
+
 async function main() {
-  const user = await prisma.user.create({ data });
+  const user = await prisma.user.create({
+    data: {
+      username: "filip",
+      email: "testfilip@mail.com",
+      passwordHash:
+        "5994471ABB01112AFCC18159F6CC74B4F511B99806DA59B3CAF5A9C173CACFC5",
+    },
+  });
+
+  console.log(user);
 }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
