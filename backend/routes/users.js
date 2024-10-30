960d531911d5cfb954d7ca7def926456658705db
filip <filip@smartiot.cloud>
2024-10-30 19:02:01 +0200

    deleted unused default package

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json({ users });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = router;
