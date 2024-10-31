ce9596b1459abaf40f52ca9e3cd6f6fac546458a
filip <filip@smartiot.cloud>
2024-10-31 12:31:42 +0200

    update show hide replies btn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
+import "./App.css";
+
 import Comment from "./components/Comment";
 import CommentForm from "./components/CommentForm";
 
-import "./App.css";
-
-const commentsData = [
+const comments = [
   {
     username: "Antony",
     date: "22.05.22 at 22:30",
     text: "I think, therefore I am",
     avatar:
       "https://gravatar.com/avatar/908cbd77ad573643714bb69ffb3feef8?s=400&d=robohash&r=x",
+    hasReplies: true, // Added flag
     replies: [
       {
         username: "Rum_8",
@@ -17,6 +18,7 @@ const commentsData = [
         text: "you know nun",
         avatar:
           "https://gravatar.com/avatar/344181407873dafec140e5d32e1a16ee?s=400&d=robohash&r=x",
+        hasReplies: false, // No replies for this reply
       },
       {
         username: "master_of_none",
@@ -24,6 +26,7 @@ const commentsData = [
         text: "doobie doobie doo",
         avatar:
           "https://gravatar.com/avatar/f4b1887dff08b78dd7e5156b089bd86f?s=400&d=robohash&r=x",
+        hasReplies: true, // Added flag
         replies: [
           {
             username: "party_maker_228",
@@ -31,6 +34,7 @@ const commentsData = [
             text: "woobie wabi boo",
             avatar:
               "https://gravatar.com/avatar/49cbdd392cf72ee38ad0b8cef9d2d8d6?s=400&d=robohash&r=x",
+            hasReplies: false, // No replies for this reply
           },
           {
             username: "mrBoombastic",
@@ -38,6 +42,7 @@ const commentsData = [
             text: "hell yeaaaa",
             avatar:
               "https://robohash.org/278bd6cddfd4080d1ec905cb56a5b3f9?set=set4&bgset=&size=400x400",
+            hasReplies: false, // No replies for this reply
           },
         ],
       },
@@ -49,6 +54,7 @@ const commentsData = [
     text: "I think, therefore I am",
     avatar:
       "https://gravatar.com/avatar/908cbd77ad573643714bb69ffb3feef8?s=400&d=robohash&r=x",
+    hasReplies: true, // Added flag
     replies: [
       {
         username: "Rum_8",
@@ -56,6 +62,7 @@ const commentsData = [
         text: "you know nun",
         avatar:
           "https://gravatar.com/avatar/344181407873dafec140e5d32e1a16ee?s=400&d=robohash&r=x",
+        hasReplies: false, // No replies for this reply
       },
       {
         username: "master_of_none",
@@ -63,6 +70,7 @@ const commentsData = [
         text: "doobie doobie doo",
         avatar:
           "https://gravatar.com/avatar/f4b1887dff08b78dd7e5156b089bd86f?s=400&d=robohash&r=x",
+        hasReplies: true, // Added flag
         replies: [
           {
             username: "party_maker_228",
@@ -70,6 +78,7 @@ const commentsData = [
             text: "woobie wabi boo",
             avatar:
               "https://gravatar.com/avatar/49cbdd392cf72ee38ad0b8cef9d2d8d6?s=400&d=robohash&r=x",
+            hasReplies: false, // No replies for this reply
           },
           {
             username: "mrBoombastic",
@@ -77,6 +86,7 @@ const commentsData = [
             text: "hell yeaaaa",
             avatar:
               "https://robohash.org/278bd6cddfd4080d1ec905cb56a5b3f9?set=set4&bgset=&size=400x400",
+            hasReplies: false, // No replies for this reply
           },
         ],
       },
@@ -87,7 +97,7 @@ const commentsData = [
 
 const App = () => (
   <div className="app">
-    {commentsData.map((comment, index) => (
+    {comments.map((comment, index) => (
       <Comment key={index} {...comment} />
     ))}
 
--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -2,7 +2,14 @@ import "./Comment.css";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
-const Comment = ({ username, date, text, avatar, replies = [] }) => {
+const Comment = ({
+  username,
+  date,
+  text,
+  avatar,
+  replies = [],
+  hasReplies = false,
+}) => {
   const [showReplies, setShowReplies] = useState(false);
 
   const toggleReplies = () => {
@@ -25,20 +32,18 @@ const Comment = ({ username, date, text, avatar, replies = [] }) => {
           <span className="user-can-click">👍</span>
           <span className="user-can-click">👎</span>
           <span className="user-can-click">💬</span>
-          <button onClick={toggleReplies} className="show-replies-btn">
-            {showReplies ? "⬆️" : "⬇️"}{" "}
-          </button>
+          {hasReplies && (
+            <button onClick={toggleReplies} className="show-replies-btn">
+              {showReplies ? "⬆️" : "⬇️"}
+            </button>
+          )}
         </div>
 
-        {replies.length > 0 && (
-          <div>
-            {showReplies && (
-              <div className="replies">
-                {replies.map((reply, index) => (
-                  <Comment key={index} {...reply} />
-                ))}
-              </div>
-            )}
+        {replies.length > 0 && showReplies && (
+          <div className="replies">
+            {replies.map((reply, index) => (
+              <Comment key={index} {...reply} />
+            ))}
           </div>
         )}
       </div>
@@ -46,19 +51,14 @@ const Comment = ({ username, date, text, avatar, replies = [] }) => {
   );
 };
 
+// PropTypes validation (optional)
 Comment.propTypes = {
   username: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  replies: PropTypes.arrayOf(
-    PropTypes.shape({
-      username: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired,
-      avatar: PropTypes.string.isRequired,
-    })
-  ),
+  replies: PropTypes.array,
+  hasReplies: PropTypes.bool,
 };
 
 export default Comment;
