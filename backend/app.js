84bc77bb1685b05cd675c15fa2d894d4705bb1b3
filip <filip@smartiot.cloud>
2024-10-31 14:06:25 +0200

    added helmet for Content Security Policy

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -11,6 +12,7 @@ var commentsRouter = require("./routes/comments");
 var app = express();
 
 app.use(logger("dev"));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
