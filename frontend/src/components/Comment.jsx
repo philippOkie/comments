f96812179443383b29316dfbc5e213bfc3e75cb3
filip <filip@smartiot.cloud>
2024-10-31 19:52:27 +0200

    not needed

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,6 +1,6 @@
 import "./Comment.css";
 
-import { useState, useRef } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 import CommentForm from "./CommentForm";
